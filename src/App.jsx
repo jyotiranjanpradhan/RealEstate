078b21750d267527f04e4ef46440d883662b21b8
Your Name Amiya <my9090202865@gmail.com>
2024-05-31 23:18:15 +0530

    roles and rights page only html and css without add role modal

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '@mdi/font/css/materialdesignicons.min.css';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./components/dashboard";
-
+import RolesandRights from "./components/rolesandRights";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/roles_right" element={<RolesandRights />} />
       </Routes>
     </BrowserRouter>
   );
