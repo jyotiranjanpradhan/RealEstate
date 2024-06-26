08caae1533f01131653486a26bf2f56b6affd22a
Aman Shaw <shawaman2384@gmail.com>
2024-06-26 10:57:18 +0530

    add-button

--- src/components/system admin/Forms/BankInfoForm.jsx
+++ src/components/system admin/Forms/BankInfoForm.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import BankSubForm from "../subForms/BankSubForm";
 
 function BankInfoForm() {
+  const [componentCount, setComponentCount] = useState(1);
+
+  function addComponent() {
+    setComponentCount(componentCount + 1);
+  }
   const { register, handleSubmit } = useForm();
 
   function onSubmit(data) {
@@ -31,121 +38,13 @@ function BankInfoForm() {
             <h5 class="card-header">Bank Info</h5>
             <div class="card-body">
               <form class="form-repeater" onSubmit={handleSubmit(onSubmit)}>
-                <div data-repeater-list="group-a">
-                  <div data-repeater-item="">
-                    <div class="row g-4">
-                      <div class="col-md-4">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="text"
-                            id="bank_name"
-                            {...register("bank_name")}
-                            name="group-a[0][Bank Name]"
-                            placeholder="Bank Name"
-                            autofocus=""
-                          />
-                          <label for="Bank Name">Bank Name</label>
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="text"
-                            id="branch_name"
-                            {...register("branch_name")}
-                            name="group-a[0][Branch Name]"
-                            placeholder="Branch Name"
-                            autofocus=""
-                          />
-                          <label for="Branch Name">Branch Name</label>
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="text"
-                            name="group-a[0][IFSC Code]"
-                            id="IFSC_code"
-                            {...register("IFSC_code")}
-                            placeholder="IFSC Code"
-                          />
-                          <label for="Company Type">IFSC Code</label>
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="text"
-                            id="account_name"
-                            {...register("account_name")}
-                            name="group-a[0][ Account Name]"
-                            placeholder=" Account Name"
-                            autofocus=""
-                          />
-                          <label for="Compay Size"> Account Name</label>
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="text"
-                            id="account_no"
-                            {...register("account_no")}
-                            name="group-a[0][Account No]"
-                            placeholder="Account No"
-                            autofocus=""
-                          />
-                          <label for="Account No">Account No</label>
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            type="file"
-                            class="form-control"
-                            id="file"
-                            {...register("file")}
-                            required=""
-                          />
-                          <label for="basic-default-upload-file">
-                            Bank Logo
-                          </label>
-                        </div>
-                      </div>
-                      <div class="col-md-4">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="text"
-                            id="account_type"
-                            {...register("account_type")}
-                            name="group-a[0][Account Type]"
-                            placeholder="Account Type"
-                            autofocus=""
-                          />
-                          <label for="Account No">Account Type</label>
-                        </div>
-                      </div>
-                      <div class="col-md-4 col-12 d-flex align-items-center mb-0">
-                        <button
-                          class="btn btn-outline-danger waves-effect"
-                          data-repeater-delete=""
-                        >
-                          <i class="mdi mdi-close me-1"></i>
-                          <span class="align-middle">Delete</span>
-                        </button>
-                      </div>
-                    </div>
-                    <hr />
-                  </div>
-                </div>
+                {componentCount &&
+                  Array.from({ length: componentCount }, (_, index) => (
+                    <BankSubForm key={index} register={register} />
+                  ))}
                 <div class="col-12 d-flex justify-content-between">
                   <button
+                    onClick={addComponent}
                     class="btn btn-primary waves-effect waves-light"
                     data-repeater-create=""
                   >
--- src/components/system admin/Forms/Board.jsx
+++ src/components/system admin/Forms/Board.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import BoardSubForm from "../subForms/BoardSubForm";
 
 function Board() {
+  const [componentCount, setComponentCount] = useState(1);
   const { register, handleSubmit } = useForm();
 
+  function addComponent() {
+    setComponentCount(componentCount + 1);
+  }
   function onSubmit(data) {
     console.log(data);
   }
@@ -31,101 +37,13 @@ function Board() {
             <h5 class="card-header">Board Directors</h5>
             <div class="card-body">
               <form class="form-repeater" onSubmit={handleSubmit(onSubmit)}>
-                <div data-repeater-list="group-a">
-                  <div data-repeater-item="">
-                    <div class="row g-4">
-                      <div class="col-md-6">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="text"
-                            id="name"
-                            {...register("name")}
-                            placeholder="Name"
-                            autofocus=""
-                          />
-                          <label for="Name">Name</label>
-                        </div>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="text"
-                            id="designation"
-                            {...register("designation")}
-                            placeholder="Designation"
-                            autofocus=""
-                          />
-                          <label for="Designation">Designation</label>
-                        </div>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="date"
-                            id="date_of_joining"
-                            {...register("date_of_joining")}
-                          />
-                          <label for="Incorporation Date">
-                            Date of Joining
-                          </label>
-                        </div>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="date"
-                            id="date_of_leaving"
-                            {...register("date_of_leaving")}
-                          />
-                          <label for="Incorporation Date">
-                            Date of Leaving
-                          </label>
-                        </div>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="text"
-                            id="share"
-                            {...register("share")}
-                            placeholder="%"
-                            autofocus=""
-                          />
-                          <label for="Incorporation No">Share</label>
-                        </div>
-                      </div>
-                      <div class="col-md-6">
-                        <div class="form-floating form-floating-outline">
-                          <input
-                            class="form-control"
-                            type="text"
-                            id="stakeholder"
-                            {...register("stakeholder")}
-                            placeholder="Stakeholder"
-                          />
-                          <label for="Incorporation Agency">Stakeholder</label>
-                        </div>
-                      </div>
-                      <div class="col-md-4 col-12 d-flex align-items-center mb-0">
-                        <button
-                          class="btn btn-outline-danger waves-effect"
-                          data-repeater-delete=""
-                        >
-                          <i class="mdi mdi-close me-1"></i>
-                          <span class="align-middle">Delete</span>
-                        </button>
-                      </div>
-                    </div>
-                    <hr />
-                  </div>
-                </div>
+                {componentCount &&
+                  Array.from({ length: componentCount }, (_, index) => (
+                    <BoardSubForm key={index} register={register} />
+                  ))}
                 <div class="col-12 d-flex justify-content-between">
                   <button
+                    onClick={addComponent}
                     class="btn btn-primary waves-effect waves-light"
                     data-repeater-create=""
                   >
