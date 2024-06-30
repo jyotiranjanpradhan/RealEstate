cd7fcfdc5fb4914b714013e04559c7869c48f66a
Aman Shaw <shawaman2384@gmail.com>
2024-06-30 22:42:55 +0530

    business-management-sidebar

--- src/ui/sidebar/Business.jsx
+++ src/ui/sidebar/Business.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import SubItems from "../sideBarItems/SubItems";
-const array = [
-  { value: "Location Master", to: "" },
+const subArray = [
   { value: "Country", to: "/businessManagement/country" },
   { value: "State", to: "/businessManagement/state" },
   { value: "City / District", to: "/businessManagement/city" },
   { value: "Area / G.P", to: "/businessManagement/areaGram" },
   { value: "Village", to: "/businessManagement/village" },
   { value: "PIN", to: "/businessManagement/pin" },
+];
+const array = [
+  { value: "Location Master", to: "" },
+
   { value: "Zone / Segment", to: "/businessManagement/zoneAndSegment" },
   { value: "Area", to: "/businessManagement/area" },
   { value: "Territory", to: "/businessManagement/territory" },
@@ -15,6 +18,7 @@ const array = [
 ];
 function Business() {
   const [show, setShow] = useState(false);
+  const [subMenu, setSubMenu] = useState(false);
   return (
     <li class={show ? "menu-item open" : "menu-item"}>
       <div
@@ -27,9 +31,27 @@ function Business() {
         <div>Business Management</div>
       </div>
       <ul class="menu-sub">
-        {array.map((el) => (
-          <SubItems value={el.value} to={el.to} />
-        ))}
+        {array.map((el) =>
+          el.value === "Location Master" ? (
+            <li class={subMenu ? "menu-item open" : "menu-item"}>
+              <div
+                onClick={() => {
+                  setSubMenu(!subMenu);
+                }}
+                class="menu-link menu-toggle waves-effect"
+              >
+                <div>Location</div>
+              </div>
+              <ul class="menu-sub">
+                {subArray.map((el, index) => (
+                  <SubItems value={el.value} to={el.to} key={index} />
+                ))}
+              </ul>
+            </li>
+          ) : (
+            <SubItems value={el.value} to={el.to} />
+          )
+        )}
       </ul>
     </li>
   );
