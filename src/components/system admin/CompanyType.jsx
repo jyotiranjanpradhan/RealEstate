a9bdf7c3e703d4f11e805ec3470fc0891685c955
Aman Shaw <shawaman2384@gmail.com>
2024-07-02 14:49:27 +0530

    company-type-api

--- src/components/system admin/CompanyType.jsx
+++ src/components/system admin/CompanyType.jsx
@@ -1,33 +1,15 @@
-import axios from "axios";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useAddCompanyType } from "../../hooks/systemAdmin/useAddCompanyType";
 
 function CompanyType() {
-  const [type, setType] = useState("");
+  const { isPending, mutate } = useAddCompanyType();
+  const { register, handleSubmit } = useForm();
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    if (!type) return;
-    // const res = await fetch(
-    //   "http://20.244.48.88:8000/system_company_type_handler/",
-    //   {
-    //     method: "POST",
-    //     body: JSON.stringify({
-    //       body: type,
-    //     }),
-    //     headers: {
-    //       "Content-type": "application/json; charset=UTF-8",
-    //     },
-    //   }
-    // );
-    const res = await axios({
-      method: "POST",
-      url: "http://20.244.48.88:8000/system_company_type_handler/",
-      data: {
-        type_name: type,
-      },
-    });
-    console.log(res);
+  function onSubmit(data) {
+    mutate(data);
+    console.log(data);
   }
+
   return (
     <>
       <h5>
@@ -39,16 +21,14 @@ function CompanyType() {
           <div className="card">
             <h5 className="card-header"></h5>
             <div className="card-body pt-1">
-              <form className="row g-3" onSubmit={handleSubmit}>
+              <form className="row g-3" onSubmit={handleSubmit(onSubmit)}>
                 <div className="col-md-4">
                   <div className="form-floating form-floating-outline">
                     <input
                       type="text"
-                      value={type}
-                      onChange={(e) => setType(e.target.value)}
                       className="form-control"
                       id="type_name"
-                      name="Company Type Name"
+                      {...register("type_name")}
                       placeholder="Company Type Name"
                     />
                     <label for="Company Type Name">Company Type Name</label>
