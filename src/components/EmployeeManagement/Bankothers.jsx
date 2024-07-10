a17dfd72a1f6c60ce14d46caf01dc1903f2f1caf
Aman Shaw <shawaman2384@gmail.com>
2024-07-10 17:17:34 +0530

    employee-api

--- src/components/EmployeeManagement/Bankothers.jsx
+++ src/components/EmployeeManagement/Bankothers.jsx
@@ -1,51 +1,32 @@
-import React from 'react';
-import './Bankothers'
+import React from "react";
+import "./Bankothers";
+import { useForm } from "react-hook-form";
+import { useAddEmBank } from "../../hooks/employeeManagement/useAddEmBank";
 const Bankothers = () => {
+  const { register, handleSubmit } = useForm();
+  const { isPending, mutate } = useAddEmBank();
+  function onSubmit(data) {
+    const formData = new FormData();
+
+    for (const [key, value] of Object.entries(data)) {
+      if (key === "proof_image") formData.append(key, value[0]);
+      else formData.append(key, value);
+    }
+    mutate(formData);
+    console.log(data);
+  }
   return (
     <div className="content-wrapper">
       <div className="container-xxl flex-grow-1 container-p-y">
         <h5>
-          <span className="text-muted fw-light">Employee Management /</span> Bank & Others
+          <span className="text-muted fw-light">Employee Management /</span>{" "}
+          Bank & Others
         </h5>
         <div className="row">
           <div className="col-12 mb-4">
             <div className="bs-stepper wizard-vertical wizard-numbered vertical mt-2">
-              <div className="bs-stepper-header gap-lg-2">
-                <div className="step" data-target="#account-details-1">
-                  <button type="button" className="step-trigger">
-                    <span className="bs-stepper-circle"><i className="mdi mdi-check"></i></span>
-                    <span className="bs-stepper-label">
-                      <span className="d-flex flex-column gap-1 ms-2">
-                        <span className="bs-stepper-title">Bank Info:</span>
-                      </span>
-                    </span>
-                  </button>
-                </div>
-                <div className="line"></div>
-                <div className="step" data-target="#personal-info-1">
-                  <button type="button" className="step-trigger">
-                    <span className="bs-stepper-circle"><i className="mdi mdi-check"></i></span>
-                    <span className="bs-stepper-label">
-                      <span className="d-flex flex-column gap-1 ms-2">
-                        <span className="bs-stepper-title">EPFO Info:</span>
-                      </span>
-                    </span>
-                  </button>
-                </div>
-                <div className="line"></div>
-                <div className="step" data-target="#social-links-1">
-                  <button type="button" className="step-trigger">
-                    <span className="bs-stepper-circle"><i className="mdi mdi-check"></i></span>
-                    <span className="bs-stepper-label">
-                      <span className="d-flex flex-column gap-1 ms-2">
-                        <span className="bs-stepper-title">Insurance Info:</span>
-                      </span>
-                    </span>
-                  </button>
-                </div>
-              </div>
               <div className="bs-stepper-content">
-                <form onSubmit={() => false}>
+                <form onSubmit={handleSubmit(onSubmit)}>
                   <div id="account-details-1" className="content">
                     <div className="content-header mb-3">
                       <h6 className="mb-0">Bank Info</h6>
@@ -53,55 +34,95 @@ const Bankothers = () => {
                     <div className="row g-4">
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="email" id="" className="form-control" placeholder="Bank Name" aria-label="" />
+                          <input
+                            type="text"
+                            id="bank_name"
+                            {...register("bank_name")}
+                            className="form-control"
+                            placeholder="Bank Name"
+                            aria-label=""
+                          />
                           <label htmlFor="email-vertical">Bank Name</label>
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="email" id="" className="form-control" placeholder="Branch Name" aria-label="" />
+                          <input
+                            type="text"
+                            id="branch_name"
+                            {...register("branch_name")}
+                            className="form-control"
+                            placeholder="Branch Name"
+                            aria-label=""
+                          />
                           <label htmlFor="email-vertical">Branch Name</label>
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="text" id="" className="form-control" placeholder="IFSC Code" aria-label="" />
+                          <input
+                            type="text"
+                            id="IFSC"
+                            {...register("IFSC")}
+                            className="form-control"
+                            placeholder="IFSC Code"
+                            aria-label=""
+                          />
                           <label htmlFor="email-vertical">IFSC Code</label>
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="text" id="" className="form-control" placeholder="Account Type" aria-label="" />
+                          <input
+                            type="text"
+                            id="account_type"
+                            {...register("account_type")}
+                            className="form-control"
+                            placeholder="Account Type"
+                            aria-label=""
+                          />
                           <label htmlFor="email-vertical">Account Type</label>
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="text" id="" className="form-control" placeholder="Account Name" aria-label="" />
+                          <input
+                            type="text"
+                            id="account_name"
+                            {...register("account_name")}
+                            className="form-control"
+                            placeholder="Account Name"
+                            aria-label=""
+                          />
                           <label htmlFor="email-vertical">Account Name</label>
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="text" id="" className="form-control" placeholder="Account No" aria-label="" />
+                          <input
+                            type="text"
+                            id="account_no"
+                            {...register("account_no")}
+                            className="form-control"
+                            placeholder="Account No"
+                            aria-label=""
+                          />
                           <label htmlFor="email-vertical">Account No</label>
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="file" className="form-control" id="basic-default-upload-file" required />
-                          <label htmlFor="basic-default-upload-file">Proof Image</label>
+                          <input
+                            type="file"
+                            className="form-control"
+                            id="proof_image"
+                            {...register("proof_image")}
+                            required
+                          />
+                          <label htmlFor="basic-default-upload-file">
+                            Proof Image
+                          </label>
                         </div>
-                      </div>
-                      <div className="col-12 d-flex justify-content-between">
-                        <button className="btn btn-outline-secondary btn-prev" disabled>
-                          <i className="mdi mdi-arrow-left me-sm-1 me-0"></i>
-                          <span className="align-middle d-sm-inline-block d-none">Previous</span>
-                        </button>
-                        <button className="btn btn-primary btn-next">
-                          <span className="align-middle d-sm-inline-block d-none me-sm-1">Next</span>
-                          <i className="mdi mdi-arrow-right"></i>
-                        </button>
                       </div>
                     </div>
                   </div>
@@ -112,31 +133,39 @@ const Bankothers = () => {
                     <div className="row g-4">
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="text" id="" className="form-control" placeholder="EPFO No" />
+                          <input
+                            type="text"
+                            id="efpo_no"
+                            {...register("efpo_no")}
+                            className="form-control"
+                            placeholder="EPFO No"
+                          />
                           <label htmlFor="first-name-vertical">EPFO No</label>
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="text" id="" className="form-control" placeholder="State" />
+                          <input
+                            type="text"
+                            id="efpo_status"
+                            {...register("efpo_status")}
+                            className="form-control"
+                            placeholder="State"
+                          />
                           <label htmlFor="last-name-vertical">State</label>
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="text" id="" className="form-control" placeholder="Branch" />
+                          <input
+                            type="text"
+                            id="efpo_branch"
+                            {...register("efpo_branch")}
+                            className="form-control"
+                            placeholder="Branch"
+                          />
                           <label htmlFor="last-name-vertical">Branch</label>
                         </div>
-                      </div>
-                      <div className="col-12 d-flex justify-content-between">
-                        <button className="btn btn-outline-secondary btn-prev">
-                          <i className="mdi mdi-arrow-left me-sm-1 me-0"></i>
-                          <span className="align-middle d-sm-inline-block d-none">Previous</span>
-                        </button>
-                        <button className="btn btn-primary btn-next">
-                          <span className="align-middle d-sm-inline-block d-none me-sm-1">Next</span>
-                          <i className="mdi mdi-arrow-right"></i>
-                        </button>
                       </div>
                     </div>
                   </div>
@@ -147,34 +176,64 @@ const Bankothers = () => {
                     <div className="row g-4">
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="text" id="" className="form-control" placeholder="Insurance No" />
-                          <label htmlFor="first-name-vertical">Insurance No</label>
+                          <input
+                            type="text"
+                            id="insurance_no"
+                            {...register("insurance_no")}
+                            className="form-control"
+                            placeholder="Insurance No"
+                          />
+                          <label htmlFor="first-name-vertical">
+                            Insurance No
+                          </label>
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="text" id="" className="form-control" placeholder="Provider" />
+                          <input
+                            type="text"
+                            id="insurance_provider"
+                            {...register("insurance_provider")}
+                            className="form-control"
+                            placeholder="Provider"
+                          />
                           <label htmlFor="last-name-vertical">Provider</label>
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="text" id="" className="form-control" placeholder="State" />
+                          <input
+                            type="text"
+                            id="insurance_state"
+                            {...register("insurance_state")}
+                            className="form-control"
+                            placeholder="State"
+                          />
                           <label htmlFor="last-name-vertical">State</label>
                         </div>
                       </div>
                       <div className="col-sm-6">
                         <div className="form-floating form-floating-outline">
-                          <input type="text" id="" className="form-control" placeholder="Branch" />
+                          <input
+                            type="text"
+                            id="insurance_branch"
+                            {...register("insurance_branch")}
+                            className="form-control"
+                            placeholder="Branch"
+                          />
                           <label htmlFor="last-name-vertical">Branch</label>
                         </div>
                       </div>
                       <div className="col-12 d-flex justify-content-between">
                         <button className="btn btn-outline-secondary btn-prev">
                           <i className="mdi mdi-arrow-left me-sm-1 me-0"></i>
-                          <span className="align-middle d-sm-inline-block d-none">Previous</span>
+                          <span className="align-middle d-sm-inline-block d-none">
+                            Previous
+                          </span>
                         </button>
-                        <button className="btn btn-primary btn-submit">Submit</button>
+                        <button className="btn btn-primary btn-submit">
+                          Submit
+                        </button>
                       </div>
                     </div>
                   </div>
@@ -186,6 +245,6 @@ const Bankothers = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Bankothers;
---
+++ src/services/apiEmployeeManagement.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export async function createEmployeeBank(data) {
+  try {
+    const res = await axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_URL_BASE}/api/employee_bank_handler/`,
+      data: data,
+    });
+    console.log(res);
+  } catch (error) {
+    console.log(error);
+    const errorMessage = Object.keys(error.response.data).join(",");
+    throw new Error(`Please provide ${errorMessage}`);
+  }
+}
